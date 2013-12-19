7ed69ad9d3b37a50b72d0d40f056065afeb5d354
Chris Dent <chris.dent@gmail.com>
2013-12-19 17:43:30 +0000

    add filenames to download links, only show if member

--- assets/backups.js
+++ assets/backups.js
@@ -1,12 +1,18 @@
 var app = angular.module('app', []);
 
 app.controller('BackupsCtrl', ['$scope', function($scope) {
-	var space = {};
-	space.name = tiddlyweb.status.space.name;
+	var space = {},
+	    name = tiddlyweb.status.space.name,
+	    timeStamp = +new Date / 1e3 | 0,
+	    isMember = tiddlyweb.status.space.recipe.match(/_private$/);
+	space.name = name;
 	space.publicBag = space.name + '_public';
 	space.privateBag = space.name + '_private';
+	space.publicFileName = space.publicBag + '-' + timeStamp + '.json';
+	space.privateFileName = space.privateBag + '-' + timeStamp + '.json';
 	space.isSelf = (space.name === 'backups');
 	space.notSelf = !space.isSelf;
+	space.isMember = isMember;
 
 	$scope.$parent.space = space;
 }]);
